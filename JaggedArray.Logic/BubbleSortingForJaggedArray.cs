aa664ae7582a4a429f98cafe9b33123b7644aed9
ivinnichek <vinnichekira@gmail.com>
2017-10-31 21:52:23 +0300

    Add variant of sorting in another class

--- JaggedArray.Logic/BubbleSortingForJaggedArray.cs
+++ JaggedArray.Logic/BubbleSortingForJaggedArray.cs
@@ -11,158 +11,23 @@ namespace JaggedArray.Logic
         #region Public Methods
 
         /// <summary>
-        /// In the order of increasing sums of elements of rows in matrix.
-        /// </summary>
-        /// <param name="lhs">One row of matrix.</param>
-        /// <param name="rhs">Another row of matrix.</param>
-        /// <returns>Negative number if sum of the next row is greater, positive number if less, and 0 if sums are equal.</returns>
-        public class ComparerSumByInc : IComparer
-        { 
-            public int Compare(int[] lhs, int[] rhs)
-            {
-                if (lhs == null)
-                {
-                    return 1;
-                }
-                if (rhs == null)
-                {
-                    return -1;
-                }
-                return lhs.Sum() - rhs.Sum();
-            }
-        }
-
-        /// <summary>
-        /// In the order of increasing sums of elements of rows in matrix.
-        /// </summary>
-        /// <param name="lhs">One row of matrix.</param>
-        /// <param name="rhs">Another row of matrix.</param>
-        /// <returns>Negative number if sum of the next row is greater, positive number if less, and 0 if sums are equal.</returns>
-        public class ComparerSumByDec : IComparer
-        { 
-            public int Compare(int[] lhs, int[] rhs)
-            {
-                if (lhs == null)
-                {
-                    return -1; 
-                }
-                if (rhs == null)
-                {
-                    return 1;
-                }
-                return rhs.Sum() - lhs.Sum();
-            }
-        }
-
-        /// <summary>
-        /// In the order of increasing sums of elements of rows in matrix.
-        /// </summary>
-        /// <param name="lhs">One row of matrix.</param>
-        /// <param name="rhs">Another row of matrix.</param>
-        /// <returns>Negative number if sum of the next row is greater, positive number if less, and 0 if sums are equal.</returns>
-        public class ComparerMaxByInc : IComparer
-        { 
-            public int Compare(int[] lhs, int[] rhs)
-            {
-                if (lhs == null)
-                {
-                    return 1;
-                }
-                if (rhs == null)
-                {
-                    return -1;
-                }
-                return lhs.Max() - rhs.Max();
-            }
-        }
-
-        /// <summary>
-        /// In the order of increasing sums of elements of rows in matrix.
-        /// </summary>
-        /// <param name="lhs">One row of matrix.</param>
-        /// <param name="rhs">Another row of matrix.</param>
-        /// <returns>Negative number if sum of the next row is greater, positive number if less, and 0 if sums are equal.</returns>
-        public class ComparerMaxByDec : IComparer
-        { 
-            public int Compare(int[] lhs, int[] rhs)
-            {
-                if (lhs == null)
-                {
-                    return -1;
-                }
-                if (rhs == null)
-                {
-                    return 1;
-                }
-                return rhs.Max() - lhs.Max();
-            }
-        }
-
-        /// <summary>
-        /// In the order of increasing min elements of rows in matrix.
-        /// </summary>
-        /// <param name="lhs">One row of matrix.</param>
-        /// <param name="rhs">Another row of matrix.</param>
-        /// <returns>Negative number if min element of the next row is greater, positive number if less, and 0 if max elements are equal.</returns>
-        public class ComparerMinByInc : IComparer
-        {
-            public int Compare(int[] lhs, int[] rhs)
-            {
-                if (lhs == null)
-                {
-                    return 1;
-                }
-                if (rhs == null)
-                {
-                    return -1;
-                }
-                return lhs.Min() - rhs.Min();
-            }
-        }
-
-        /// <summary>
-        /// In the order of increasing min elements of rows in matrix.
-        /// </summary>
-        /// <param name="lhs">One row of matrix.</param>
-        /// <param name="rhs">Another row of matrix.</param>
-        /// <returns>Negative number if min element of the next row is greater, positive number if less, and 0 if max elements are equal.</returns>
-        public class ComparerMinByDec : IComparer
-        {
-            public int Compare(int[] lhs, int[] rhs)
-            {
-                if (lhs == null)
-                {
-                    return 1;
-                }
-                if (rhs == null)
-                {
-                    return -1;
-                }
-                return rhs.Min() - lhs.Min();
-            }
-        }
-
-        /// <summary>
         /// Sort jagged array using bubble sorting by different criteria.
         /// </summary>
         /// <param name="jaggedArray">Jagged array.</param>
         /// <param name="comparer">Selected class of sorting among suitable.</param>
-        public static class ArrayHelper
+        public static void BubbleSorting(int[][] jaggedArray, IComparer comparer)
         {
-            public static void BubbleSorting(int[][] jaggedArray, IComparer comparer)
+            if (jaggedArray == null)
             {
-                if (jaggedArray == null)
+                throw new ArgumentNullException(nameof(jaggedArray));
+            }
+            for (int i = 1; i < jaggedArray.Length; i++)
+            {
+                for (int j = 0; j < jaggedArray.Length - 1; j++)
                 {
-                    throw new ArgumentNullException(nameof(jaggedArray));
-                }
-                for (int i = 1; i < jaggedArray.Length; i++)
-                {
-                    for (int j = 0; j < jaggedArray.Length - 1; j++)
+                    if (comparer.Compare(jaggedArray[j], jaggedArray[j + 1]) > 0)
                     {
-                        if (comparer.Compare(jaggedArray[j], jaggedArray[j + 1]) > 0)
-                        {
-                            Swap(ref jaggedArray[j], ref jaggedArray[j + 1]);
-                        }
+                        Swap(ref jaggedArray[j], ref jaggedArray[j + 1]);
                     }
                 }
             }
